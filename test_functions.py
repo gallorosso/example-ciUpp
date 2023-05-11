a2a943f3c68253bc0b4b561a4bc52f8266b37963
Andrea Gallo Rosso <gallorosso@users.noreply.github.com>
2023-05-11 13:57:01 +0200

    Corrected the import from functions.py to function.py

--- test_functions.py
+++ test_functions.py
@@ -1,6 +1,6 @@
 
-from functions import add, subtract, multiply
-from functions import convert_fahrenheit_to_celsius as f2c
+from function import add, subtract, multiply
+from function import convert_fahrenheit_to_celsius as f2c
 import pytest
 
 def test_add():
